17481f924b3a130d3e2a1ba6da4b098a7531bdfd
kanagaraj <suryakmk4@gmail.com>
2024-05-20 13:20:48 +0530

    Files added

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy  # Removed import (not used)
 
 app = Flask(__name__)
 
-# Removed database configuration (not used)
-
 app.config['PREFERRED_URL_SCHEME'] = 'https'
-
-# db = SQLAlchemy(app)  # Removed (not used)
 
 from app import routes
